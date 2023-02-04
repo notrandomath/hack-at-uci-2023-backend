7cc9f0dc2fb76866af73a70ba1ee055c209457dd
m-tsoi <mjtsoi@uci.edu>
2023-02-04 11:43:27 -0800

    n,m
    jbhnj

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3999;
 
 app.use(express.json());
 
-const db = require("./models");
+const { Sequelize } = require("sequelize-cockroachdb");
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 // Routers
 const twilioRouter = require("./routes/Twilio");
 app.use("/twilio", twilioRouter);
 
-db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-});
